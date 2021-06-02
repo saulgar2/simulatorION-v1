12a7dcd51af26469080ad9afef119356d9813c9a
saulgar2 <saulgar2@gmail.com>
2021-06-01 23:47:32 -0500

    actualizacion

--- cypress/integration/ION-simulator-v1.js
+++ cypress/integration/ION-simulator-v1.js
@@ -1,4 +1,9 @@
 context("Simulator Test", () => {
+
+    /*before(() =>{
+        cy.fixture('inputData.json').as('input')
+    })*/
+
     beforeEach(() => {
         cy.visit('/')
         /*cy.fixture("inputData").then((data) => {
@@ -7,10 +12,15 @@ context("Simulator Test", () => {
     })
 
     it("Liquidity Income", () => {
-        //cy.dataIncome(this.data.money_do_i_need, this.inputData.property_collateral, this.inputData.monthly_income);
-        //cy.dataUser(this.inputData.name, this.inputData.phone, this.inputData.email);
-        cy.dataIncome('1000000', '1500000', '80000');
-        cy.dataUser('pruebas testing qa', '5512345698', 'pruebas@pruebas.com');
+        //const datos = this.input;
+        //cy.get(datos).each((dataObject) =>{
+            //cy.dataIncome(this.data.money_do_i_need, this.inputData.property_collateral, this.inputData.monthly_income);
+            //cy.dataUser(this.inputData.name, this.inputData.phone, this.inputData.email);
+            cy.dataIncome('1000000', '1500000', '80000');
+            cy.dataUser('pruebas testing qa', '5512345698', 'pruebas@pruebas.com');
+        //})
+
+        
     })
 
 })
