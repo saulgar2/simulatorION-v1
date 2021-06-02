c6ea19e4011cadf04e13c6c0b18c17992ad8278e
saulgar2 <saulgar2@gmail.com>
2021-06-01 22:20:19 -0500

    actualizacion

--- cypress/integration/ION-simulator-v1.ts
+++ cypress/integration/ION-simulator-v1.ts
@@ -1,15 +1,15 @@
 context("Simulator Test", () => {
     beforeEach(() => {
         cy.visit('/')
-        /*cy.fixture("inputData").then((data) => {
+        cy.fixture("inputData").then((data) => {
       this.data = data
-    })*/
+    })
     })
 
     it("Liquidity Income", () => {
         //cy.dataIncome(this.data.money_do_i_need, this.inputData.property_collateral, this.inputData.monthly_income);
         //cy.dataUser(this.inputData.name, this.inputData.phone, this.inputData.email);
-        cy.dataIncome('1000000', '1500000', '80000');
+        cy.dataIncome(this.data.money_do_i_need, '1500000', '80000');
         cy.dataUser('pruebas testing qa', '5512345698', 'pruebas@pruebas.com');
     })
 
